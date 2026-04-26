a0c3e2b636d6560dfe2c3bf0e63e6eb7a7e6528b
superuser <amirkhalilzain@gmail.com>
2026-04-26 06:41:58 +0000

    Permulaan pemrograman asikron

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -1,8 +1,17 @@
 import 'data/data.dart';
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
     printUsage();
+
+    Future<String> getWikipediaArticle(String articleTitle) async {
+      final url = Uri.https(
+        'en.wikipedia.org', // Wikipedia API domain
+        '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
+      );
+      // ...
+    }
   } else if (arguments.first == 'version') {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
@@ -13,8 +22,16 @@ void main(List<String> arguments) {
   }
 }
 
+//
+//
+//
+//
+//
+//
+//
+//
+//
 // Jika sudah berhasil memahami bagaimana cara kerja CLI ini,
 // cobalah untuk membuat CLI yang dapat menampilkan daftar proyek opensource yang sedang trending di GitHub,
 // lalu buatlah perintah untuk menampilkan detail dari proyek tersebut,
-// seperti jumlah bintang, jumlah fork, dan bahasa pemrograman yang digunakan. 
-
+// seperti jumlah bintang, jumlah fork, dan bahasa pemrograman yang digunakan.
