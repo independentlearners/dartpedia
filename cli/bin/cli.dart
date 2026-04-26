efa2fd231388aab65f10f4d6cab4c63b84c6358b
superuser <amirkhalilzain@gmail.com>
2026-04-26 02:21:42 +0000

    Memasuki pembelajaran pemrograman asikron

--- cli/bin/cli.dart
+++ cli/bin/cli.dart
@@ -7,8 +7,14 @@ void main(List<String> arguments) {
     print('Dartpedia CLI version $version');
   } else if (arguments.first == 'search') {
     final inputArgs = arguments.length > 1 ? arguments.sublist(1) : null;
-    searcWikipedia(inputArgs);
+    searchWikipedia(inputArgs);
   } else {
     printNothing(arguments);
   }
 }
+
+// Jika sudah berhasil memahami bagaimana cara kerja CLI ini,
+// cobalah untuk membuat CLI yang dapat menampilkan daftar proyek opensource yang sedang trending di GitHub,
+// lalu buatlah perintah untuk menampilkan detail dari proyek tersebut,
+// seperti jumlah bintang, jumlah fork, dan bahasa pemrograman yang digunakan. 
+
